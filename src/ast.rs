5588cc992c87264920a99786413d519e09bf0da5
eckon <niklas.meyer95@hotmail.de>
2024-03-03 20:38:41 +0100

    feat(parser): implement let/return statement

--- src/ast.rs
+++ src/ast.rs
@@ -5,17 +5,21 @@ pub enum Statement {
     Let {
         token: Token,
         name: Identifier,
-        value: Node,
+        // value: Node,
+    },
+    Return {
+        token: Token,
+        // value: Node,
     },
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Identifier {
     token: Token,
     value: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Node {
     token: Token,
 }
@@ -32,7 +36,25 @@ impl Program {
         }
     }
 
+    pub fn add_statement(&mut self, statement: Statement) {
+        self.statements.push(statement);
+    }
+
     pub fn get_statements(&self) -> &Vec<Statement> {
         &self.statements
+    }
+}
+
+impl Identifier {
+    pub fn new(token: Token, value: String) -> Identifier {
+        Identifier { token, value }
+    }
+
+    pub fn get_token(&self) -> &Token {
+        &self.token
+    }
+
+    pub fn get_value(&self) -> &String {
+        &self.value
     }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,51 @@
-use crate::{ast::Program, lexer::Lexer, token::Token};
+use std::fmt;
+
+use crate::{
+    ast::{Identifier, Program, Statement},
+    lexer::Lexer,
+    token::{Token, TokenType},
+};
+
+#[derive(Debug)]
+pub struct ParserError {
+    message: String,
+    token: Token,
+}
+
+#[derive(Debug)]
+pub struct ParserErrorList {
+    errors: Vec<ParserError>,
+}
+
+impl ParserErrorList {
+    pub fn new() -> ParserErrorList {
+        ParserErrorList {
+            errors: Vec::<ParserError>::new(),
+        }
+    }
+
+    pub fn add_error(&mut self, error: ParserError) {
+        self.errors.push(error);
+    }
+
+    pub fn get_errors(&self) -> &Vec<ParserError> {
+        &self.errors
+    }
+}
+
+impl fmt::Display for ParserErrorList {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for e in self.get_errors() {
+            writeln!(f, "msg: {}, token: {:?}", e.message, e.token)?
+        }
+
+        Ok(())
+    }
+}
 
 #[derive(Debug, Default, Clone)]
 pub struct Parser {
     lexer: Lexer,
-
     current_token: Token,
     next_token: Token,
 }
@@ -27,16 +69,128 @@ impl Parser {
         self.next_token = self.lexer.next_token();
     }
 
-    pub fn parse_program(self) -> Program {
-        Program::new()
+    fn expect_next_token(&mut self, expected_type: TokenType) -> Result<(), ParserError> {
+        if self.next_token.get_type() == expected_type {
+            self.next_token();
+            Ok(())
+        } else {
+            Err(ParserError {
+                message: format!(
+                    "Expected next token to be of type \"{:?}\", but got \"{:?}\"",
+                    expected_type,
+                    self.next_token.get_type()
+                ),
+                token: self.next_token.clone(),
+            })
+        }
+    }
+
+    fn parse_statement(&mut self) -> Result<Statement, ParserError> {
+        match self.current_token.get_type() {
+            TokenType::Let => self.parse_let_statement(),
+            TokenType::Return => self.parse_return_statement(),
+            n => Err(ParserError {
+                message: format!("Unknown token type for statement: {:?}", n),
+                token: self.current_token.clone(),
+            }),
+        }
+    }
+
+    fn parse_let_statement(&mut self) -> Result<Statement, ParserError> {
+        let token = self.current_token.clone();
+
+        self.expect_next_token(TokenType::Ident)?;
+
+        let identifier = Identifier::new(
+            self.current_token.clone(),
+            self.current_token.get_literal().clone(),
+        );
+
+        self.expect_next_token(TokenType::Assign)?;
+
+        // for now, just skip the expression
+        loop {
+            self.next_token();
+            if self.current_token.get_type() == TokenType::Semicolon {
+                break;
+            }
+        }
+
+        Ok(Statement::Let {
+            token,
+            name: identifier,
+        })
+    }
+
+    fn parse_return_statement(&mut self) -> Result<Statement, ParserError> {
+        let token = self.current_token.clone();
+
+        // for now, just skip the expression
+        loop {
+            self.next_token();
+            if self.current_token.get_type() == TokenType::Semicolon {
+                break;
+            }
+        }
+
+        Ok(Statement::Return { token })
+    }
+
+    pub fn parse_program(mut self) -> Result<Program, ParserErrorList> {
+        let mut program = Program::new();
+        let mut errors = ParserErrorList::new();
+
+        loop {
+            let statement = self.parse_statement();
+            match statement {
+                Ok(statement) => program.add_statement(statement),
+                Err(e) => errors.add_error(e),
+            }
+
+            self.next_token();
+            if self.current_token.get_type() == TokenType::Eof {
+                break;
+            }
+        }
+
+        if errors.get_errors().is_empty() {
+            Ok(program)
+        } else {
+            Err(errors)
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::lexer::Lexer;
-
     use super::Parser;
+    use crate::{ast::Statement, lexer::Lexer};
+
+    #[test]
+    fn test_return_statement() {
+        let input = "
+            return \"foo\";
+            return 5;
+            return add(1, 3);
+        ";
+
+        let lexer = Lexer::new(input.into());
+        let parser = Parser::new(lexer);
+        let program = parser.parse_program();
+
+        if let Err(errors) = program {
+            println!("{}", errors);
+            panic!("Program could not be parsed correctly");
+        }
+
+        let program = program.unwrap();
+
+        assert_eq!(program.get_statements().len(), 3);
+
+        for s in program.get_statements() {
+            assert!(matches!(s, Statement::Return { .. }));
+        }
+    }
 
     #[test]
     fn test_let_statements() {
@@ -48,9 +202,25 @@ mod tests {
 
         let lexer = Lexer::new(input.into());
         let parser = Parser::new(lexer);
-
         let program = parser.parse_program();
 
-        println!("{:?}", program);
+        if let Err(errors) = program {
+            println!("{}", errors);
+            panic!("Program could not be parsed correctly");
+        }
+
+        let program = program.unwrap();
+
+        assert_eq!(program.get_statements().len(), 3);
+
+        let expected_identifier = ["x", "y", "foobar"];
+
+        for (i, s) in program.get_statements().iter().enumerate() {
+            assert!(matches!(s, Statement::Let { .. }));
+
+            if let Statement::Let { name, .. } = s {
+                assert_eq!(expected_identifier[i], name.get_value());
+            }
+        }
     }
 }
